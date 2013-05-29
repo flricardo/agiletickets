69fc51a8a7a075e975867963d52ef6f6ee193ff2
Flavio Ferreira <flavio.ferreira@tre-ma.jus.br>
2013-05-29 10:23:01 -0300

    Inclusao de classe de teste do JPAEspeaculoDao

---
+++ src/test/java/br/com/caelum/agiletickets/persistencia/JPAEspetaculoDaoTest.java
@@ -0,0 +1,16 @@
+package br.com.caelum.agiletickets.persistencia;
+
+import org.junit.Test;
+
+import br.com.caelum.agiletickets.domain.Relogio;
+import br.com.caelum.agiletickets.domain.RelogioDoSistema;
+
+public class JPAEspetaculoDaoTest extends DataBaseTest{
+	
+	@Test
+	public void testName() throws Exception {		
+		Relogio relogio = new RelogioDoSistema();
+		JPAEspetaculoDao dao = new JPAEspetaculoDao(manager, relogio);		
+	}
+	
+}
