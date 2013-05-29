5c633c515e17bfbeef122a59144d75778838fa54
Flavio Ferreira <flavio.ferreira@tre-ma.jus.br>
2013-05-29 10:25:42 -0300

    inclusao de classe abstrata das entityManager de teste

---
+++ src/test/java/br/com/caelum/agiletickets/persistencia/DataBaseTest.java
@@ -0,0 +1,25 @@
+package br.com.caelum.agiletickets.persistencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class DataBaseTest {
+	private EntityManagerFactory factory;
+	protected EntityManager manager;
+	
+	@Before
+	public void comeco(){
+		factory = Persistence.createEntityManagerFactory("test");
+		manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+	}
+	
+	@After
+	public void fim(){
+		manager.getTransaction().rollback();
+	}
+}
