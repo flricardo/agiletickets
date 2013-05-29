43d176b55a3f2f4b534ddd3ef54ec2759f0170d7
Flavio Ferreira <flavio.ferreira@tre-ma.jus.br>
2013-05-29 11:18:42 -0300

    teste

--- src/test/java/br/com/caelum/agiletickets/persistencia/DataBaseTest.java
+++ src/test/java/br/com/caelum/agiletickets/persistencia/DataBaseTest.java
@@ -1,13 +1,16 @@
 package br.com.caelum.agiletickets.persistencia;
 
+import static org.junit.Assert.*;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
-public abstract class DataBaseTest {
+public class DataBaseTest {
 	private EntityManagerFactory factory;
 	protected EntityManager manager;
 	
@@ -22,4 +25,9 @@ public abstract class DataBaseTest {
 //	public void fim(){
 //		manager.getTransaction().rollback();
 //	}
+	
+	@Test
+	public void testName() throws Exception {
+		
+	}
 }
--- src/test/java/br/com/caelum/agiletickets/persistencia/JPAEspetaculoDaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/persistencia/JPAEspetaculoDaoTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 import br.com.caelum.agiletickets.domain.Relogio;
 import br.com.caelum.agiletickets.domain.RelogioDoSistema;
 
-public class JPAEspetaculoDaoTest extends DataBaseTest{
+public class JPAEspetaculoDaoTest {
 	
 	@Test
 	public void testName() throws Exception {		
 		Relogio relogio = new RelogioDoSistema();
-		JPAEspetaculoDao dao = new JPAEspetaculoDao(manager, relogio);		
+		//JPAEspetaculoDao dao = new JPAEspetaculoDao(manager, relogio);		
 	}
 	
 }
